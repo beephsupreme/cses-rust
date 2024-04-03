78b425b96e6a921e22774b6ebf46ce03eba6feb6
Michael Rowsey <mike@rowsey.org>
2024-04-03 11:16:07 -0700

    experiment with custom error, tweak docs

--- src/bin/weird_algorithm.rs
+++ src/bin/weird_algorithm.rs
@@ -7,9 +7,12 @@
 use cses::solutions::weird_algorithm::solve;
 use cses::utils::io::{get_int, vector_to_string};
 
-/// Driver for the solution to the "Weird Algorithm" problem https://cses.fi/problemset/task/1068
+/// Driver for the "Weird Algorithm" problem https://cses.fi/problemset/task/1068
 fn main() {
     let n: u64 = get_int();
-    let v: Vec<u64> = solve(n);
-    println!("{} ", vector_to_string(v, Some(" ")));
+    let v= solve(n);
+    match v {
+        Ok(v) => println!("{} ", vector_to_string(v, Some(" "))),
+        Err(e) => eprintln!("{}", e),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
  * Licensed under the MIT license (http://opensource.org/licenses/MIT)
  * This file may not be copied, modified, or distributed except according to those terms.
  */
-
 fn main() {
     println!("CSES Problem Set");
 }
