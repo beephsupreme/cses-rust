2c807400f4f3229be6c9962e1bf8af6f658e706a
Michael Rowsey <mike@rowsey.org>
2024-04-07 14:34:57 -0700

    WIP trailing_zeros

--- src/solutions/trailing_zeros.rs
+++ src/solutions/trailing_zeros.rs
@@ -4,14 +4,37 @@
  * This file may not be copied, modified, or distributed except according to those terms.
  */
 
-pub fn solve(n: u64) -> u64 {
+use std::ops::RangeInclusive;
+
+use anyhow::Result;
+
+use crate::utils::error::CsesError;
+
+const VALID_RANGE: RangeInclusive<u64> = 1..=1_000_000_000;
+
+/// Solution to (https://cses.fi/problemset/task/1618)
+/// Calculate the number of trailing zeros in the factorial of a number.
+/// # Parameters
+/// * n: u64 - The number to calculate the factorial of.
+/// # Returns
+/// A u64 representing the number of trailing zeros in the factorial of n.
+pub fn solve(n: u64) -> Result<u64, CsesError> {
+    if !VALID_RANGE.contains(&n) {
+        let msg = format!(
+            "n is {} but expected {} ≤ n ≤ {}",
+            n,
+            VALID_RANGE.start(),
+            VALID_RANGE.end()
+        );
+        return Err(CsesError::InvalidInput(msg));
+    }
     let mut result: u64 = 0;
     let mut i: u64 = 5;
     while n / i >= 1 {
         result += n / i;
         i *= 5;
     }
-    result
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -20,22 +43,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn unit_01() {
-        assert_eq!(solve(395), 97);
+    fn trailing_zeros_unit_0() {
+        let expected: CsesError =
+            CsesError::InvalidInput("n is 0 but expected 1 ≤ n ≤ 1000000000".to_string());
+        assert_eq!(solve(0), Err(expected));
     }
 
     #[test]
-    fn unit_02() {
-        assert_eq!(solve(871), 215);
+    fn trailing_zeros_unit_1000000001() {
+        let expected: CsesError =
+            CsesError::InvalidInput("n is 1000000001 but expected 1 ≤ n ≤ 1000000000".to_string());
+        assert_eq!(solve(1000000001), Err(expected));
+    }
+    #[test]
+    fn trailing_zeros_unit_395() {
+        assert_eq!(solve(395).unwrap(), 97);
     }
 
     #[test]
-    fn unit_03() {
-        assert_eq!(solve(239), 57);
+    fn trailing_zeros_unit_871() {
+        assert_eq!(solve(871).unwrap(), 215);
     }
 
     #[test]
-    fn unit_04() {
-        assert_eq!(solve(535), 132);
+    fn trailing_zeros_unit_239() {
+        assert_eq!(solve(239).unwrap(), 57);
+    }
+
+    #[test]
+    fn trailing_zeros_unit_535() {
+        assert_eq!(solve(535).unwrap(), 132);
     }
 }
--- tests/trailing_zeros_integration.rs
+++ tests/trailing_zeros_integration.rs
@@ -4,22 +4,33 @@
  * This file may not be copied, modified, or distributed except according to those terms.
  */
 
+use std::fs::File;
+use std::io::BufReader;
+
+use anyhow::Result;
 use log::info;
 
 use cses::solutions::trailing_zeros::solve;
 use cses::utils::integration_setup::get_test_filenames;
-use cses::utils::io::file_to_int;
+use cses::utils::io::{get_token, load_all_tokens};
 
 #[cfg(test)]
 #[test]
-fn integration_01() {
+fn trailing_zeros_integration() -> Result<()> {
     env_logger::init();
     let (questions, answers) = get_test_filenames("trailing_zeros");
-    (0..questions.len()).for_each(|i| {
+    for i in 0..questions.len() {
         info!("{}: {}", i + 1, questions[i]);
-        let a: u64 = file_to_int(&answers[i]);
-        let n: u64 = file_to_int(&questions[i]);
-        let r: u64 = solve(n);
+        let mut q_reader = BufReader::new(File::open(&questions[i])?);
+        let mut a_reader = BufReader::new(File::open(&answers[i])?);
+        let mut q_buffer: String = String::new();
+        let mut a_buffer: String = String::new();
+        let mut q_tokens = load_all_tokens(&mut q_reader, &mut q_buffer)?;
+        let mut a_tokens = load_all_tokens(&mut a_reader, &mut a_buffer)?;
+        let n: u64 = get_token(&mut q_tokens)?;
+        let a: u64 = get_token(&mut a_tokens)?;
+        let r: u64 = solve(n)?;
         assert_eq!(r, a);
-    });
+    }
+    Ok(())
 }
